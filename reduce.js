6bd9f89d5641a2f59d92c5ffe6eff5fddba3af2a
Karl Bohn <karl@McBain>
2023-10-17 21:08:46 -0400

    vowelCount working

--- reduce.js
+++ reduce.js
@@ -1,11 +1,3 @@
-/*
-Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
-
-Examples:
-    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
-    extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
-*/
-
 function extractValue(arr, key) {
     return arr.reduce(function(accum, nextObj){
         accum.push(nextObj[key]);
@@ -13,18 +5,20 @@ function extractValue(arr, key) {
     },[])
 }
 
-/*
-Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
 
-Examples:
-    vowelCount('Elie') // {e:2,i:1};
-    vowelCount('Tim') // {i:1};
-    vowelCount('Matt') // {a:1})
-    vowelCount('hmmm') // {};
-    vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
-*/
-
-function vowelCount(str) {}
+function vowelCount(str) {
+    let arr = Array.from(str);
+    let vowels = 'aeiouAEIOU';
+    return arr.reduce(function(accum, next){
+        let lowercase = next.toLowerCase()
+        if (vowels.indexOf(lowercase) !== -1){
+            if (accum[lowercase]) {accum[lowercase]++;
+            }
+            else accum[lowercase] = 1;
+        }
+        return accum;
+    },{}) 
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
